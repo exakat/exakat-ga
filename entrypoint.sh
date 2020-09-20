5481e9af1e0ed154c84d1751b485fa923d176ff0
Famille Seguy <famille@NeoMacBookPro.local>
2020-09-20 08:18:41 +0200

    version 2.1.8, with Sarif added

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,13 @@ export EXAKAT_IGNORE_RULES=$INPUT_IGNORE_RULES
 export EXAKAT_IGNORE_DIRS=$INPUT_IGNORE_DIRS
 export EXAKAT_INCLUDE_DIRS=$INPUT_INCLUDE_DIRS
 
+if [ ! -z "$INPUT_PROJECT_REPORTS" ]
+then
+	if [ $INPUT_PROJECT_REPORTS = 'Sarif' ]
+	then
+    		sed -i s/Perfile/Sarif/ config/exakat.ini 
+	fi
+fi
+
 ln -s /github/workspace projects/review/code
 exakat project -p review
